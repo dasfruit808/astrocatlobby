94b7b2a6ab6507ee42896a9b7818a02420fa05f8
dasfruit808 <lionelkdeguzman@yahoo.com>
2025-09-22 10:08:55 -0700

    Delete astrocatlobby directory

--- astrocatlobby/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Astrocat Lobby package."""
-from .models import AstroCat
-from .services import LobbyService
-from .controllers import LobbyController
-
-__all__ = ["AstroCat", "LobbyService", "LobbyController"]
-__version__ = "0.1.0"
